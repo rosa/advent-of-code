81c984bdb9854fccf9801bc483c0452ed1b1516e
Rosa Gutierrez <rosa@37signals.com>
2025-12-02 16:00:43 +0100

    Start 2025 and implement day 01 in Elixir

---
+++ 2025/day01.exs
@@ -0,0 +1,35 @@
+# --- Day 1: Secret Entrance ---
+
+defmodule Rotations do
+  def zero_dials_count(path), do: lines(path) |> zero_dials_count(50, 0, 0)
+
+  def zero_dials_count([], 0, count, passes), do: {count + 1, passes + 1}
+  def zero_dials_count([], _, count, passes), do: {count, passes}
+  def zero_dials_count([rot|rotations], 0, count, passes), do: zero_dials_count(rotations, rotate(rot, 0), count + 1, passes + pass(rot, 0) + 1)
+  def zero_dials_count([rot|rotations], current, count, passes), do: zero_dials_count(rotations, rotate(rot, current), count, passes + pass(rot, current))
+
+  def rotate({"L", n}, current), do: Integer.mod(current - n, 100)
+  def rotate({"R", n}, current), do: Integer.mod(current + n, 100)
+
+  def pass({"L", n}, current), do: Integer.floor_div(n, 100) + pass(current, current - Integer.mod(n, 100))
+  def pass({"R", n}, current), do: Integer.floor_div(n, 100) + pass(current, current + Integer.mod(n, 100))
+  def pass(current, n) when current > 0 and n < 0, do: 1
+  def pass(_, n) when n > 100, do: 1
+  def pass(_, _), do: 0
+
+  defp lines(path) do
+    File.read!(path)
+    |> String.split(~r{\n}, trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    [_, dir, count] = Regex.run(~r/(L|R)(\d+)/, line)
+    {dir, String.to_integer(count)}
+  end
+end
+
+# Rotations.zero_dials_count("inputs/input01.txt") |> elem(0) |> IO.puts
+
+# --- Part Two ---
+Rotations.zero_dials_count("inputs/input01.txt") |> elem(1) |> IO.puts
